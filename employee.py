d6ab44e812d22c1742a929979aece5342f4176c6
Denisio Rordrigues <denisio@ymail.com>
2023-03-21 01:49:15 -0300

    Criando o objeto de empregado

---
+++ employee.py
@@ -0,0 +1,7 @@
+class Employee:
+    def __init__(self, name, cpf, nit, birth_date, mothers_name) -> None:
+        name = name
+        cpf = cpf
+        nit = nit
+        birth_date = birth_date
+        mothers_name = mothers_name
