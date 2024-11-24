ecbf70aa4d55c18801699cde8cd4f6a2f2935efe
Denisio Rordrigues <denisio@ymail.com>
2024-11-24 20:06:09 -0300

    Experimento de obter as tabelas do arquivo PDF

--- textract.py
+++ textract.py
@@ -2,26 +2,39 @@ import boto3
 import json
 
 from pdf2image import convert_from_path
+
 from PIL import Image
 
 pdfFilePath = 'files/extrato.pdf'
-tiffFilePath = 'files/extrato.tiff'
+#tiffFilePath = 'files/extrato.tiff'
+
+# pages = convert_from_path(pdfFilePath, dpi=300)
+
+# for i, page in enumerate(pages):
+#     page.save(f'page_{i + 1}.png', 'PNG')
 
 # Converter PDF para imagens
 # pages = convert_from_path(pdfFilePath, dpi=300)
 
 # Salvar como um único TIFF
-# pages[0].save(tiffFilePath, save_all=True, append_images=pages[1:])
+#pages[0].save(tiffFilePath, save_all=True, append_images=pages[1:])
 
-# Cliente AWS Textract
+#Cliente AWS Textract
 textract = boto3.client('textract')
 
 
-# Carregar o documento
-with open(pdfFilePath, "rb") as document:
-    print(document)
+#Carregar o documento
+# with open(pdfFilePath, "rb") as document:
+#     print(document)
+#     response = textract.analyze_document(
+#         Document={'Bytes': document.read()},
+#         FeatureTypes=["TABLES"]
+#     )
+
+with open(pdfFilePath, 'rb') as document_file:
+    document_bytes = document_file.read()
     response = textract.analyze_document(
-        Document={'Bytes': document.read()},
+        Document={'Bytes': document_bytes},
         FeatureTypes=["TABLES"]
     )
 
